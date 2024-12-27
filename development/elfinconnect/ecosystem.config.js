6cc7a02dcd4a3f992870f4816521f93f49d3c6fe
Aranyalma2 <40268481+Aranyalma2@users.noreply.github.com>
2024-12-27 21:25:43 +0100

    fix envs

--- development/elfinconnect/ecosystem.config.js
+++ development/elfinconnect/ecosystem.config.js
@@ -4,36 +4,12 @@ module.exports = {
     name   : "web",
     cwd    : "./elfinConnectWeb/",
     script : "./index.js",
-    env_production: {
-      NODE_ENV: "production",
-      PORT: 3000,
-      DATABASE: "database:27017",
-      DATABASE_COLLECTION: "elfinconnect",
-      GATEWAY_SERVER_IP: "localhost",
-      GATEWAY_SERVER_PORT: 3001,
-      GATEWAY_SERVER_TIMEOUT: 30000
-    },
-    env_development: {
-      NODE_ENV: "development",
-      PORT: 3000,
-      DATABASE: "database:27017",
-      DATABASE_COLLECTION: "elfinconnect",
-      GATEWAY_SERVER_IP: "localhost",
-      GATEWAY_SERVER_PORT: 3001,
-      GATEWAY_SERVER_TIMEOUT: 30000
-    },
     exec_mode : "fork"
   },
   {
     name   : "bridge",
     cwd    : "./elfinConnectServer/",
     script : "./index.js",
-    env_production: {
-      NODE_ENV: "production"
-    },
-    env_development: {
-      NODE_ENV: "development"
-    },
     exec_mode : "fork"
   },
 ]
--- production/elfinconnect/ecosystem.config.js
+++ production/elfinconnect/ecosystem.config.js
@@ -4,24 +4,6 @@ module.exports = {
     name   : "web",
     cwd    : "./elfinConnectWeb/",
     script : "./index.js",
-    env_production: {
-      NODE_ENV: "production",
-      PORT: 3000,
-      DATABASE: "database:27017",
-      DATABASE_COLLECTION: "elfinconnect",
-      GATEWAY_SERVER_IP: "localhost",
-      GATEWAY_SERVER_PORT: 3001,
-      GATEWAY_SERVER_TIMEOUT: 30000
-    },
-    env_development: {
-      NODE_ENV: "development",
-      PORT: 3000,
-      DATABASE: "database:27017",
-      DATABASE_COLLECTION: "elfinconnect",
-      GATEWAY_SERVER_IP: "localhost",
-      GATEWAY_SERVER_PORT: 3001,
-      GATEWAY_SERVER_TIMEOUT: 30000
-    },
     exec_mode : "cluster",
     instances : "max"
   },
@@ -29,12 +11,6 @@ module.exports = {
     name   : "bridge",
     cwd    : "./elfinConnectServer/",
     script : "./index.js",
-    env_production: {
-      NODE_ENV: "production"
-    },
-    env_development: {
-      NODE_ENV: "development"
-    },
     exec_mode : "cluster",
     instances : "max"
   },
